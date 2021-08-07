6992434efe40ff6fa26768fcfe8a8a40099ce47b
Deblina Das <77379919+DeblinaD@users.noreply.github.com>
2021-08-07 20:44:30 +0530

    Update bitManipulation1.cpp
    added set bit part

--- bitManipulation1.cpp
+++ bitManipulation1.cpp
@@ -1,4 +1,4 @@
-//get bit code
+//get bit and set bit code
 #include<bits/stdc++.h>
 using namespace std;
 
@@ -7,8 +7,15 @@ int getbit(int n, int position)
     return((n & (1<<position))!=0);
 }
 
+int setbit(int n, int position)
+{
+    return ((n | (1<<position));
+}
 int main()
 {
     cout<<getbit(9,2)<<endl;
     //if output comes 0 that means the position had 0 other wise 1
+    
+    cout<<setbit(5,1)<<endl;
+    //it gives the output as the number which is finally created. just in this case it gives output 7.
 }
