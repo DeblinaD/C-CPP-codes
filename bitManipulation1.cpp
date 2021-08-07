d3c4e0e02135607e7882c6b715d29dea781c154c
Deblina Das <77379919+DeblinaD@users.noreply.github.com>
2021-08-07 19:55:57 +0530

    Add files via upload

---
+++ bitManipulation1.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int getbit(int n, int position) 
+{
+    return((n & (1<<position))!=0);
+}
+
+int main()
+{
+    cout<<getbit(9,2)<<endl;
+    //if output comes 0 that means the position had 0 other wise 1
+}
